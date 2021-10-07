3e83c8850e42df9d6782aa1bb2031e31663fb92e
Camila-Roa <mariacamilaroa@outlook.es>
2021-10-07 13:36:57 -0500

    commit inicial

---
+++ Corte_2/Actividad_ 6/Ejercicio_4/Ejercicio_4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+/*
+    GROUP MEMBERS
+        Roa Ruiz Maria Camila
+        Barbosa Fierro Luis Esteban
+        Cardona Acuña David Esteban
+        González Casilimas Daniel Stevan
+    
+    SHIFT
+        diurnal
+
+    GROUP
+        63102C / 239-2A */
+namespace AplicacionBase
+{
+    class Ejercicio_4
+    {
+        
+        static void Main(string[] args)
+        {
+          Console.WriteLine("======================================================");
+          Console.WriteLine("ESTA ES SU PRIMERA LISTA:");
+           ArrayList Listnum1 = new ArrayList();
+           Listnum1.Add(11);
+           Listnum1.Add(13);
+           Listnum1.Add(15);
+           Listnum1.Add(17);
+           Listnum1.Add(19);
+
+           imprime(Listnum1);
+
+          Console.WriteLine("ESTA ES SU SEGUNDA LISTA:");
+           ArrayList Listnum2 = new ArrayList();
+           Listnum2.Add(12);
+           Listnum2.Add(14);
+           Listnum2.Add(16);
+           Listnum2.Add(18);
+           Listnum2.Add(20);
+
+           imprime(Listnum2);
+
+           Console.WriteLine("=====================================================");
+           Console.WriteLine("LA CONCATENACION DE AMBAS LISTAS ES: ");
+           Listnum1.AddRange(Listnum2);
+           imprime(Listnum1);
+
+        }
+        static void imprime (ArrayList arreglo){
+            foreach(int nums in arreglo)
+            {
+             Console.Write(" {0} ", nums);
+             Console.WriteLine("\n=====================================================");
+            }
+        }
+    }
+}
---
+++ Corte_2/Actividad_ 6/Ejercicio_9/Ejercicio_9.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+/*
+    GROUP MEMBERS
+        Roa Ruiz Maria Camila
+        Barbosa Fierro Luis Esteban
+        Cardona Acuña David Esteban
+        González Casilimas Daniel Stevan
+    
+    SHIFT
+        diurnal
+
+    GROUP
+        63102C / 239-2A */
+
+namespace AplicacionBase
+{
+    class Ejercicio_9
+    {
+        static void Main(string[] args)
+        {
+            
+            ArrayList Pila = new ArrayList();
+            int Tamaño1;
+            Console.WriteLine("Por favor ingrese el rango de la pila: ");
+            Tamaño1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("========================================");
+            Console.WriteLine("SU PILA ES: ");
+
+            Random Aleatorios1 = new Random();
+            for(int j = 0; j < Tamaño1; j++)
+            {
+                Pila.Add(Aleatorios1.Next(1,100));
+            }
+          imprime(Pila);
+
+           ArrayList Pila2 = new ArrayList();
+           Console.WriteLine("SU COPIA DE SU PILA ES LA SIGUIENTE: ");
+
+           Pila2 = Pila;
+           imprime(Pila2);
+        }
+        static void imprime (ArrayList arreglo){
+            foreach(int nums in arreglo)
+            {
+             Console.Write($"[{nums}]");
+            }
+            Console.WriteLine("\n==============");
+        }    
+    }
+}
