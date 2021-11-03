bf5ce73eb07c2d2c277395527f70cc3c5ec28fb3
Camila-Roa <mariacamilaroa@outlook.es>
2021-11-03 08:55:48 -0500

    commit inicial

---
+++ Corte_3/ACTIVIDAD 9/Ejercicio_2/Ejercicio_2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+/*
+    GROUP MEMBERS
+        Roa Ruiz Maria Camila
+        Barbosa Fierro Luis Esteban
+        Cardona Acuña David Esteban
+        González Casilimas Daniel Stevan
+    
+    SHIFT
+        diurnal
+
+    GROUP
+        63102C / 239-2A */
+
+namespace AplicacionBase
+{
+    //Implementar la clase operaciones. Se deben cargar dos valores enteros en el constructor, calcular su suma, resta, 
+    //multiplicación y división, cada una en un método, imprimir dichos resultados.
+
+    class Operaciones
+    {
+        private int NumeroA = 0;
+        private int NumeroB = 0;
+
+        public Operaciones()
+        {
+            Console.WriteLine("CONSTRUCTOR");
+            Console.WriteLine("Ingrese el primer valor");
+            NumeroA = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Ingrese el segundo valor");
+            NumeroB = int.Parse(Console.ReadLine()); 
+        }
+
+        public void Sumar()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"La suma de A y B es: {NumeroA + NumeroB}");
+        }
+
+        public void Restar()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"La resta de A y B es: {NumeroA - NumeroB}");
+        }
+
+        public void Multiplicar()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"La multiplicación de A y B es: {NumeroA * NumeroB}");
+        }
+
+        public void Dividir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"La división de A y B es: {NumeroA / NumeroB}");
+        }
+        
+        static void Main(string[] args)
+        {
+            Operaciones Ca = new Operaciones();
+            Ca.Sumar();
+            Ca.Restar();
+            Ca.Multiplicar();
+            Ca.Dividir();
+            Console.ReadKey();
+        }
+    }
+}
